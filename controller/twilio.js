3a15401fd71c019cf08c3edeabee78439dbaf953
gmatsss <gmaturan60@gmail.com>
2024-05-01 13:15:08 +0800

    update to email

--- controller/twilio.js
+++ controller/twilio.js
@@ -5,6 +5,7 @@ const VoiceResponse = twilio.twiml.VoiceResponse;
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
+const nodemailer = require("nodemailer");
 
 exports.handleIncomingCall = (req, res) => {
   // Set the timezone to Central Time (Arkadelphia, AR)
@@ -26,9 +27,44 @@ exports.handleIncomingCall = (req, res) => {
   } else {
     console.log("Directing to voicemail due to outside business hours.");
     response.say("Please leave a message after the beep.");
-    response.record({ maxLength: 30 });
+    response.record({
+      maxLength: 30,
+      playBeep: true,
+      recordingStatusCallback: "/recording-completed",
+    });
   }
 
   res.type("text/xml");
   res.send(response.toString());
+};
+
+exports.handleRecordingCompleted = async (req, res) => {
+  const recordingUrl = req.body.RecordingUrl;
+
+  const transporter = nodemailer.createTransport({
+    host: "mail.linkage.ph",
+    secure: true,
+    auth: {
+      user: "gabriel.maturan@linkage.ph",
+      pass: "Linkage2023",
+    },
+  });
+
+  const mailOptions = {
+    from: '"Gabriel Maturan" <gabriel.maturan@linkage.ph>',
+    to: "gabriel.maturan@linkage.ph, gmaturan60@gmail.com",
+    subject: "New Voicemail Received",
+    text: `You have a new voicemail: ${recordingUrl}`,
+    html: `<b>You have a new voicemail:</b> <a href="${recordingUrl}">${recordingUrl}</a>`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Error sending email: " + error.message);
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
 };
--- routes/twilio.js
+++ routes/twilio.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { handleIncomingCall } = require("../controller/twilio");
+const {
+  handleIncomingCall,
+  handleRecordingCompleted,
+} = require("../controller/twilio");
 
 router.post("/incoming", handleIncomingCall);
+router.post("/recording-completed", handleRecordingCompleted);
 
 module.exports = router;
