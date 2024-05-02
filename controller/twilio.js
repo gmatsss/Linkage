d48e42b38386d0f0cd3cf35603e9cca0c34f97d8
gmatsss <gmaturan60@gmail.com>
2024-05-02 13:06:12 +0800

    update

--- controller/twilio.js
+++ controller/twilio.js
@@ -62,8 +62,9 @@ exports.handleRecordingCompleted = async (req, res) => {
   });
 
   const mailOptions = {
+    //need there email provider
     from: '"Linkage" <gabriel.maturan@linkage.ph>',
-    to: "gabriel.maturan@linkage.ph, gmaturan60@gmail.com", //hpmurphy@icloud.com
+    to: "gabriel.maturan@linkage.ph, hpmurphy@icloud.com",
 
     subject: "New Voicemail Received",
     text: `You have a new voicemail from ${callerNumber}: ${recordingUrl}`,
