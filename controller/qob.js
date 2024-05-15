de26b4a7365c6d061cafa9c7ec57320baa2bb96a
gmatsss <gmaturan60@gmail.com>
2024-05-15 14:01:03 +0800

    update

--- controller/qob.js
+++ controller/qob.js
@@ -94,10 +94,15 @@ const checkopportunityfields = async (req, res) => {
     const missingFields = fieldsToCheck.filter((field) => !req.body[field.key]);
 
     if (missingFields.length > 0) {
+      const missingFieldsLabels = missingFields.map((field) => field.label);
+      const message = `Missing required fields: ${missingFieldsLabels.join(
+        ", "
+      )}`;
+
       return res.status(200).json({
         success: false,
-        message: "Missing required fields",
-        missingFields: missingFields.map((field) => field.label),
+        message: message,
+        missingFields: missingFieldsLabels,
       });
     }
 
