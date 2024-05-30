a9615dd9146481356dee72df1ff6011f1269fc3a
gmatsss <gmaturan60@gmail.com>
2024-05-30 14:07:58 +0800

    zxc

--- model/SalesForceSalesOrder.js
+++ model/SalesForceSalesOrder.js
@@ -3,19 +3,15 @@ const mongoose = require("mongoose");
 const itemSchema = new mongoose.Schema({
   sku: {
     type: String,
-    required: true,
   },
   quantity: {
     type: Number,
-    required: true,
   },
 });
 
 const salesForceSalesOrderSchema = new mongoose.Schema({
   id: {
     type: String,
-    required: true,
-    unique: true,
   },
   time: {
     type: Date,
@@ -23,8 +19,6 @@ const salesForceSalesOrderSchema = new mongoose.Schema({
   },
   name: {
     type: String,
-    required: true,
-    unique: true,
   },
   items: [itemSchema], // Adding items array to the schema
 });
