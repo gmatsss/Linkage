c0748c4f35f4efdf1939d63267f7ff6290941d33
gmatsss <gmaturan60@gmail.com>
2024-05-30 15:23:16 +0800

    asd

--- model/SalesForceSalesOrder.js
+++ model/SalesForceSalesOrder.js
@@ -16,14 +16,14 @@ const itemSchema = new mongoose.Schema({
 });
 
 const salesForceSalesOrderSchema = new mongoose.Schema({
-  id: {
+  Opportunity_id: {
     type: String,
   },
   time: {
     type: Date,
     default: Date.now,
   },
-  name: {
+  Opportunity_name: {
     type: String,
   },
   items: [itemSchema], // Adding items array to the schema
