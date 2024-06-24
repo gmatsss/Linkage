554826808403cc735a8509006383bc42294a7181
gmatsss <gmaturan60@gmail.com>
2024-06-24 10:53:38 +0800

    xzc

--- routes/Koala.js
+++ routes/Koala.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { koalaArticles } = require("../controller/Koala");
 
-router.get("/createarticel", koalaArticles);
+router.post("/createarticel", koalaArticles);
 
 module.exports = router;
