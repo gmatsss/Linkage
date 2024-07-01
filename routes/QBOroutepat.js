cf46a222ddf6333f4e15e509b54339a695629ab5
gmatsss <gmaturan60@gmail.com>
2024-07-01 11:08:30 +0800

    update

--- routes/QBOroutepat.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controller/authQBO");
-
-router.get("/callback", authController.handleCallback);
-// Route to initiate OAuth flow
-router.get("/auth", authController.initiateAuth);
-
-module.exports = router;
