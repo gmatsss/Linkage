f40ca2ff349c8900aac3e2faa2cdfef3db0d6504
gmatsss <gmaturan60@gmail.com>
2024-07-01 10:46:15 +0800

    new

--- controller/authQBO.js
+++ controller/authQBO.js
@@ -1,21 +1,20 @@
-const { oauthClient } = require("../utils/oauthQBO");
-const { saveToken } = require("../utils/oauthQBO");
+const { oauthClient, getToken, saveToken } = require("../utils/oauthQBO");
+
+exports.initiateAuth = (req, res) => {
+  const authUri = oauthClient.authorizeUri({
+    scope: [OAuthClient.scopes.Accounting, OAuthClient.scopes.OpenId],
+    state: "intuit_csrf_token", // CSRF protection token
+  });
+  res.redirect(authUri);
+};
 
 exports.handleCallback = async (req, res) => {
   try {
-    const token = await oauthClient.createToken(req.url);
+    const token = await oauthClient.createToken(req.query);
     saveToken(token.getJson());
     res.send("Token obtained and saved successfully");
   } catch (err) {
     console.error("Error handling callback:", err);
     res.status(500).send("Error handling callback");
   }
-};
-
-exports.initiateAuth = (req, res) => {
-  const authUri = oauthClient.authorizeUri({
-    scope: [OAuthClient.scopes.Accounting, OAuthClient.scopes.OpenId],
-    state: "intuit_csrf_token",
-  });
-  res.redirect(authUri);
 };
--- utils/oauthQBO.js
+++ utils/oauthQBO.js
@@ -48,4 +48,5 @@ const getToken = async () => {
 module.exports = {
   oauthClient,
   getToken,
+  saveToken,
 };
