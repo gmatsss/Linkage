8f92f7a99105ad7d5824f62a12514943e1cc3652
gmatsss <gmaturan60@gmail.com>
2024-10-02 10:37:44 +0800

    delete

--- routes/gmb.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { triggernewpost, latestpost } = require("../controller/gmbs");
-//route
-router.get("/triggernewpost", triggernewpost);
-router.post("/latestpost", latestpost);
-
-module.exports = router;
