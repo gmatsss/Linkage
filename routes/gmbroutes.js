68ca409bb7b92b5da238b6b48bef48ba6b2d8a2d
gmatsss <gmaturan60@gmail.com>
2024-10-02 09:57:11 +0800

    Updated gmb.js, GmbPosts.js, and gmbroutes.js

--- routes/gmbroutes.js
+++ routes/gmbroutes.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getAuthUrl,
-  handleOAuth2Callback,
-  checkNewPosts,
+  // getAuthUrl,
+  // handleOAuth2Callback,
+  // checkNewPosts,
+  triggernewpost,
+  latestpost,
 } = require("../controller/gmb");
 
-// Route to start the OAuth2 flow
-router.get("/auth", getAuthUrl);
+// // Route to start the OAuth2 flow
+// router.get("/auth", getAuthUrl);
 
-// Route to handle the OAuth2 callback (Google will redirect here)
-router.get("/oauth2callback", handleOAuth2Callback);
+// // Route to handle the OAuth2 callback (Google will redirect here)
+// router.get("/oauth2callback", handleOAuth2Callback);
 
-// Route to check for new GMB posts
-router.get("/check-new-posts", checkNewPosts);
+// // Route to check for new GMB posts
+// router.get("/check-new-posts", checkNewPosts);
+
+router.get("/triggernewpost", triggernewpost);
+router.post("/latestpost", latestpost);
 
 module.exports = router;
